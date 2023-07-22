72ad4cb632de104b1003ec3d7f93672fdc2d6955
CachorroVoador <87171245+CachorroVoador@users.noreply.github.com>
2023-07-22 18:59:34 -0300

    Implemented a basic hitbox system. I removed the line that checked if the animation was playing in the physics_process, as it was unnecessary and replaced it with a signal that the animation player plays when it finishes playing some animation

--- Player/PlayerScripts/player.gd
+++ Player/PlayerScripts/player.gd
@@ -3,6 +3,7 @@ extends CharacterBody3D
 @onready var camera_mount = $camera_mount
 @onready var animation_player = $visuals/Constance/AnimationPlayer
 @onready var visuals = $visuals
+@onready var _hitbox: Area3D = $visuals/Constance/Armature/Skeleton3D/hitbox
 
 var SPEED = 3.0
 const JUMP_VELOCITY = 4.5
@@ -24,23 +25,24 @@ func _ready():
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 func _input(event):
+	_camera_movement(event)
+
+func _camera_movement(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
 		rotate_y(deg_to_rad(-event.relative.x * sens_horizontal))
 		camera_mount.rotate_x(deg_to_rad(-event.relative.y * sens_vertical))
+		camera_mount.rotation.x = clamp(camera_mount.rotation.x, -0.741765, 0.741765) #Clamp's the camera rotation
 		if animation_player.current_animation == "idle" or "Attack":
 			visuals.rotate_y(deg_to_rad(event.relative.x * sens_horizontal))
 
 func _physics_process(delta):
-	
-	if !animation_player.is_playing():
-		is_locked = false
-	
 	if Input.is_action_just_pressed("Attack") and is_on_floor():
-		if animation_player.current_animation != "Attack":
-			animation_player.play("Attack")
-			is_locked = true
-	
-	
+		if animation_player.current_animation == "Attack": return
+		is_locked = true
+		animation_player.play("Attack")
+		await get_tree().create_timer(.9,false).timeout
+		_hitbox.show()
+		_hitbox.monitoring = true
 	
 	# Add the gravity.
 	if not is_on_floor():
@@ -49,8 +51,8 @@ func _physics_process(delta):
 	# Handle Jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		if !is_locked:
-			if animation_player.current_animation != "jump":
-				animation_player.play("jump")
+			if animation_player.current_animation == "jump": return
+			animation_player.play("jump")
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
@@ -83,3 +85,12 @@ func _physics_process(delta):
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 	if !is_locked:
 		move_and_slide()
+
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	_hitbox.hide()
+	_hitbox.monitoring = false
+	is_locked = false
+
+func _on_hitbox_body_entered(body: Node3D) -> void:
+	body.queue_free()
